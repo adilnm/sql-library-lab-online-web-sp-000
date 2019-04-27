bda24400ee09b8253a544881af0bf104592aebea
ADIL NAIMI <adilnm23@hotmail.com>
2019-04-27 10:44:33 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -22,7 +22,7 @@ end
 def select_series_title_with_most_human_characters
   "SELECT series.title, count(*) FROM series 
     JOIN characters ON series.id=characters.series_id 
-    WHERE characters.species = "human"
+    WHERE characters.species = 'human'
     GROUP BY series.title
     ORDER BY count(*) DESC
     LIMIT 1"
