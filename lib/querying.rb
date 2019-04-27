b25fed88613a4603b09ea58420f811d80153c0a0
ADIL NAIMI <adilnm23@hotmail.com>
2019-04-27 10:44:19 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -20,7 +20,12 @@ JOIN subgenres ON series.subgenre_id=subgenres.id"
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title, count(*) FROM series 
+    JOIN characters ON series.id=characters.series_id 
+    WHERE characters.species = "human"
+    GROUP BY series.title
+    ORDER BY count(*) DESC
+    LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
